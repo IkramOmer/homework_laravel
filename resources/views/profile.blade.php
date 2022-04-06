d0d8e892339e0a60bed4980caf6b19b2103d3b64
IkramOmer <ikramomer20@gmail.com>
2022-04-06 19:48:34 +0300

    upload

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/profile.css">
+    <title>profile</title>
+</head>
+<body>
+<img src="image/ikram.jpg" alt="ikram" width="150" height="150">
+<h3>i am ikram <br> student of IT department</h3>
+</body>
+</html>
